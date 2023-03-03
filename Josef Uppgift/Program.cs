755365c38d12fa5e20b9233dbc80c8528034a73e
Elev <Elev@TEB125110>
2023-03-03 14:35:37 +0100

    Add project files.

---
+++ Josef Uppgift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace inlämning
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Addera(9, 13, 50);
+            Console.WriteLine(Addera(9,13,50));
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+        
+        
+
+    }
+}
